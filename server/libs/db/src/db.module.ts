c15a7863bb6c85ed0c8dcde2e28a65fd0376b41c
liny <linyang1020@163.com.cn>
2020-11-05 23:40:57 +0800

    init project

---
+++ server/libs/db/src/db.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { DbService } from './db.service';
+import { TypegooseModule } from 'nestjs-typegoose'
+
+@Module({
+  imports: [
+    TypegooseModule.forRoot('mongodb://localhost/blogfullstack', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+  ],
+  providers: [DbService],
+  exports: [DbService],
+})
+export class DbModule {}
